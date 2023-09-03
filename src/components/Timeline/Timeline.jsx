d79ce3cfb37e38d121f05b75c990ad4487053bd7
siddanth-6365 <114846048+siddanth-6365@users.noreply.github.com>
2023-09-03 13:05:15 +0000

    added timeline page

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,11 +1,104 @@
-import React from 'react'
-import './Timeline.css'
-const Timeline = () => {
-  return (
-    <div>
-        <h1>Timeline</h1>
-    </div>
-  )
-}
+import React from "react";
+import "./Timeline.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
-export default Timeline
+const Timeline = () => {
+
+
+
+// add objects to this array 
+  const TimelineObjects = [
+    {
+      title: "Registration begins",
+      time: "6.00 PM",
+      date: "10th SEPT",
+    },
+    {
+      title: "HACKATHON STARTS",
+      time: "5.00 PM",
+      date: "23RD SEPT",
+    },
+    {
+      title: "HACKATHON ENDS",
+      time: "5.00 PM",
+      date: "24th SEPT",
+    },
+    {
+      title: "RESULTS",
+      time: "6.00 PM",
+      date: "24th SEPT",
+    },
+  ];
+
+
+  return (
+    <div className="timelineDiv">
+      <a
+        href="https://www.sih.gov.in/sih2023PS"
+        style={{ textDecoration: "none" }}
+      >
+        {" "}
+        <div className="head1">
+          For SIH '22' Problem statements click here{" "}
+        </div>{" "}
+      </a>
+
+      <h1
+        style={{
+          textTransform: "uppercase",
+          fontWeight: 700,
+          letterSpacing: "1px",
+          fontSize: window.innerWidth > 786 ? "48px" : "30px",
+        }}
+      >
+        Timeline
+      </h1>
+
+      <div>
+        <VerticalTimeline lineColor="black">
+          {TimelineObjects.map((data, index) => (
+            <VerticalTimelineElement
+              className={`vertical-timeline-element--work ${
+                index % 2 === 0 ? "even" : "odd"
+              }`}
+              contentStyle={{
+                background: " rgba(120, 120, 120, 0.2)",
+                color: "black",
+              }}
+              contentArrowStyle={{ borderRight: "15px solid  black" }}
+              date={data.date}
+              iconStyle={{ background: "#FABB09", color: "black" }}
+              icon={
+                <h2
+                  style={{
+                    marginTop: window.innerWidth > 786 ? "20px" : "9px",
+                  }}
+                >
+                  {index + 1}{" "}
+                </h2>
+              }
+            >
+              <h1
+                style={{
+                  textTransform: "uppercase",
+                  fontSize: window.innerWidth > "786px" ? "50px" : "28px",
+                }}
+              >
+                {data.title}
+              </h1>
+              <h3>{data.time}</h3>
+
+              <p>{data.para}</p>
+            </VerticalTimelineElement>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </div>
+  );
+};
+
+export default Timeline;

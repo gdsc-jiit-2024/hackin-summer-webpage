b283daebea1ba9904fadfc77b2ded00a01617915
lonewolf1503 <harshmathur1503@gmail.com>
2023-09-04 19:11:01 +0530

    3d model attached

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Suspense } from 'react';
+import Attraction from './components/Attraction/Attraction';
+import Mainpage from './components/Mainpage/Mainpage';
 import About from './components/About/About';
-import Mainpage from './components/Mainpage/Mainpage.jsx';
-import Timeline from './components/Timeline/Timeline';
-import Tracks from './components/Tracks/Tracks';
-import Form from './components/Form/Form';
 import Countdown from './components/countdown/Countdown';
 function App() {
-  return (
+    createRoot(document.getElementById("root")).render(
+
     <div className="App">
       <Mainpage />
       <About />
+      <Suspense fallback={null}>
+        <Attraction />
+      </Suspense>
       <Countdown />
-      {/* <Tracks />
-      <Timeline />
-      <Form /> */}
     </div>
   );
 }
